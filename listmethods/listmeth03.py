194271cfeb859c6c1ac2b425022ea69ef77f52d2
Nick Scherer <nscherer30@gmail.com>
2021-01-04 22:26:46 +0000

    about complex lists

---
+++ listmethods/listmeth03.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+icecream= ["flavors", "salty"]
+icecream.append(99)
+name = input("Whats your name?").strip()
+print(f'{icecream[2]} {icecream[0]}, and {name} chooses to be {icecream[1]}')
+
