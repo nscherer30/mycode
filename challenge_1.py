c508fe8cd682d4ac6ae17f5e2db3b6c2da4ffa42
Nick Scherer <nscherer30@gmail.com>
2021-01-04 21:46:25 +0000

    manipulating lists

---
+++ challenge_1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+user_name = input("What is your name?")
+day_of_week = input("What is the day of the week?")
+print("Hello, ", user_name.strip(), "!", " Happy ", day_of_week.strip(), "!", sep="")
