d16e5028ef4c4085b9084d65c7ad89e8b3b7244c
Nick Scherer <nscherer30@gmail.com>
2021-01-06 18:40:18 +0000

    parse file with ip and success attempts

--- attemptlogin/large_fileparse-with.py
+++ attemptlogin/large_fileparse-with.py
@@ -2,15 +2,20 @@
 
 # parse keystone.common.wsgi and return number of failed login attempts
 loginfail = 0 # counter for fails
-
+loginSuccess = 0 # counter for successes
+ipaddr = -1
 # open the file for reading
 with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
 
     # loop over the file
     for line in kfile:
+        
         # if this 'fail pattern' appears in the line...
         if "- - - - -] Authorization failed" in line:
             loginfail += 1 # this is the same as loginfail = loginfail + 1
+            ipaddr = line.split(" ")[-1]
+        elif "-] Authorization failed" in line:
+            loginSuccess += 1
 
-print("The number of failed log in attempts is", loginfail)
-
+print(f"The number of failed log in attempts from {ipaddr} is {loginfail}")
+print("Successful login attempts", loginSuccess)
